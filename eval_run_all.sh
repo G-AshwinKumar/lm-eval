fd104febc212dda35fbd95ffae23131de556315a
7jvh0KFmH2CsF6niyD/04w3LIclhg0mUkkY4DNcCkSY= <pablo.martin@bsc.es>
2024-01-31 17:23:15 +0100

    bias 0-shot script

--- eval_run_all.sh
+++ eval_run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the MODEL_NAME variable
-MODEL_NAME="Mixtral-8x7B-v0.1"
+MODEL_NAME="openchat_3.5"
 
 export MODEL_NAME
 
@@ -10,6 +10,8 @@ sbatch_dir="sbatch_scripts/"
 
 # Submit each sbatch script by name
 #sbatch "${sbatch_dir}sbatch_eval_all_med.sh"
-sbatch "${sbatch_dir}sbatch_vllm.sh"
+sbatch "${sbatch_dir}sbatch_vllm_bias0.sh"
+# sbatch "${sbatch_dir}sbatch_vllm_med_and_bias0.sh"
+# sbatch "${sbatch_dir}sbatch_vllm_med_and_bias5.sh"
 
 echo "All sbatch scripts submitted."
