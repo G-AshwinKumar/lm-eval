ed7017bc72dcebce095d67904126ad3f8734a0cc
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-02-07 16:39:03 +0100

    Big Refactor: removed everything but our tasks.

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set the directory where sbatch scripts are located
-sbatch_dir="sbatch_scripts/"
-
-# Submit each sbatch script by name
-sbatch "${sbatch_dir}sbatch_eval_all_med.sh"
-
-echo "All test sbatch scripts submitted."
