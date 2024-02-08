ed2a962587570f97557cd21fbfed34fb93d1d612
PabloAgustin <pablo.martin@bsc.es>
2024-02-08 12:41:18 +0100

    modified cache

--- new_eval_all.sh
+++ new_eval_all.sh
@@ -8,8 +8,8 @@
 #SBATCH --gres=gpu:a100:1   # Request 2 GPU of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
 #SBATCH -c 32               # Cores per task requested
-#SBATCH -t 03:00:00         # Run time (hh:mm:ss) - 30 min
-#SBATCH --mem=120G          # Memory per node
+#SBATCH -t 00:06:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH --mem=247G          # Memory per node
 
 MODEL_NAME="openchat_3.5"
 
@@ -18,7 +18,7 @@ MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$M
 
 module load singularity/3.9.7
 singularity exec -B /mnt -B /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/lm-evaluation-harness:/home/kike/llm-evaluation-harness --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm_cuda118_bias.sif \
-    bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/hf_cache_pablo" && \
+    bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/hf_cache_'${User}'" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
     --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=auto,gpu_memory_utilization=0.9 \
