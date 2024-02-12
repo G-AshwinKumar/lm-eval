1999811d92c7beedeacd685639d70d60694a8443
PabloAgustin <pabloagustinquemas@gmail.com>
2024-02-12 15:06:57 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> test ethics bug 2

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -5,13 +5,13 @@
 #SBATCH -J eval            # Job name
 #SBATCH -o slurm_output/out.txt # Name of stdout output file(%j expands to jobId)
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
-#SBATCH --gres=gpu:a100:2   # Request 1 GPU of 2 available on an average A100 node
+#SBATCH --gres=gpu:a100:1   # Request 1 GPU of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
-#SBATCH -c 64               # Cores per task requested
+#SBATCH -c 32               # Cores per task requested
 #SBATCH -t 06:00:00         # Run time (hh:mm:ss)
-#SBATCH --mem=246G          # Memory per node
+#SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="Qwen1.5-14B"
+MODEL_NAME="meditron-7b"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
