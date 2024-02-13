e24df7acdc043db99c8095a02e71d2a8d52794ba
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-02-13 11:56:19 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> test hendrycks_ethics issue

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -24,7 +24,7 @@ singularity exec -B /mnt -B $CURRENT_DIR/tasks:/home/heka_eval/llm-evaluation-ha
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
     --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.9 \
-    --tasks truthfulqa_mc2,crows_pairs,hendrycks_ethics,mmlu,medqa_4options,medqa,medqa5,medqa_template,medqa5_template,medmcqa,medmcqa_test,medmcqa_val,medmcqa_template,medmcqa_val_template,pubmedqa \
+    --tasks truthfulqa_mc2,crows_pairs,mmlu,medqa_4options,medqa,medqa5,medqa_template,medqa5_template,medmcqa,medmcqa_test,medmcqa_val,medmcqa_template,medmcqa_val_template,pubmedqa \
     --device cuda \
     --batch_size auto:4 \
     --num_fewshot 0'
