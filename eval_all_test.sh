f94fcec1b51981c0990c9a0907ec001c9c20b351
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-02-14 18:46:57 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> openchat_3.5 single-gpu

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -5,13 +5,13 @@
 #SBATCH -J eval            # Job name
 #SBATCH -o slurm_output/out.txt # Name of stdout output file(%j expands to jobId)
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
-#SBATCH --gres=gpu:a100:2   # Request 1 GPU of 2 available on an average A100 node
+#SBATCH --gres=gpu:a100:1   # Request 1 GPU of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
-#SBATCH -c 64               # Cores per task requested
+#SBATCH -c 32               # Cores per task requested
 #SBATCH -t 06:00:00         # Run time (hh:mm:ss)
 #SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="Mistral-7B-v0.1"
+MODEL_NAME="openchat_3.5"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -23,8 +23,8 @@ singularity exec -B /mnt -B $CURRENT_DIR/tasks:/home/heka_eval/llm-evaluation-ha
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     CUDA_LAUNCH_BLOCKING=1 TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
-    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=2,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.70 \
-    --tasks truthfulqa_mc2,crows_pairs,mmlu,medqa_4options,medqa,medqa5,medqa_template,medqa5_template,medmcqa,medmcqa_test,medmcqa_val,medmcqa_template,medmcqa_val_template,pubmedqa \
+    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.8 \
+    --tasks toxigen_generation_asian,truthfulqa_mc2,crows_pairs,mmlu,medqa_4options,medqa,medqa5,medqa_template,medqa5_template,medmcqa,medmcqa_test,medmcqa_val,medmcqa_template,medmcqa_val_template,pubmedqa \
     --device cuda \
     --batch_size auto:4 \
     --num_fewshot 0'
