34a314b5c2f7a74c0c2384efe2a886dc983c865e
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-02-15 10:07:44 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> fix toxigen issue

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -5,9 +5,9 @@
 #SBATCH -J eval            # Job name
 #SBATCH -o slurm_output/out.txt # Name of stdout output file(%j expands to jobId)
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
-#SBATCH --gres=gpu:a100:2   # Request 1 GPU of 2 available on an average A100 node
+#SBATCH --gres=gpu:a100:1   # Request 1 GPU of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
-#SBATCH -c 64               # Cores per task requested
+#SBATCH -c 32               # Cores per task requested
 #SBATCH -t 06:00:00         # Run time (hh:mm:ss)
 #SBATCH --mem=247G          # Memory per node
 
@@ -23,7 +23,7 @@ singularity exec -B /mnt -B $CURRENT_DIR/tasks:/home/heka_eval/llm-evaluation-ha
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     CUDA_LAUNCH_BLOCKING=1 TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
-    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=2,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.85 \
+    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.85 \
     --tasks toxigen_generation_asian,toxigen_generation_black,toxigen_generation_chinese,toxigen_generation_jewish,toxigen_generation_latino,toxigen_generation_lgbtq,toxigen_generation_mental_dis,toxigen_generation_mexican,toxigen_generation_middle_east,toxigen_generation_muslim,toxigen_generation_native_american,toxigen_generation_physical_dis,toxigen_generation_women,truthfulqa_mc2,crows_pairs,mmlu,medqa_4options,medqa,medqa5,medqa_template,medqa5_template,medmcqa,medmcqa_test,medmcqa_val,medmcqa_template,medmcqa_val_template,pubmedqa \
     --device cuda \
     --batch_size auto:4 \
