0ee6f909ca42cb7e684a8ea3041b64cc940cd864
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-02-19 22:56:16 +0100

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias5.sh> Qwen1.5-14B med and bias 0-shot

--- sbatch_vllm_med_and_bias0.sh
+++ sbatch_vllm_med_and_bias0.sh
@@ -5,13 +5,13 @@
 #SBATCH -J eval            # Job name
 #SBATCH -o slurm_output/out.txt # Name of stdout output file(%j expands to jobId)
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
-#SBATCH --gres=gpu:a100:1   # Request 1 GPU of 2 available on an average A100 node
+#SBATCH --gres=gpu:a100:2   # Request 2 GPU of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
-#SBATCH -c 32               # Cores per task requested
+#SBATCH -c 64               # Cores per task requested
 #SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="openchat_3.5"
+MODEL_NAME="Qwen1.5-14B"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -23,7 +23,7 @@ singularity exec -B /mnt -B $CURRENT_DIR/tasks:/home/heka_eval/llm-evaluation-ha
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
-    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.85 \
+    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=2,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.85 \
     --tasks toxigen_generation,toxigen_generation_asian,toxigen_generation_black,toxigen_generation_chinese,toxigen_generation_jewish,toxigen_generation_latino,toxigen_generation_lgbtq,toxigen_generation_middle_east,toxigen_generation_mental_dis,toxigen_generation_mexican,toxigen_generation_muslim,toxigen_generation_native_american,toxigen_generation_physical_dis,toxigen_generation_women,bold,bold_american_actors,bold_american_actresses,bold_asian_americans,bold_buddhism,bold_islam,bold_left_wing,bold_hispanic_and_latino_americans,bold_european_americans,bold_african_americans,bold_judaism,bold_atheism,bold_christianity,bold_sikhism,bold_hinduism,truthfulqa_mc2,crows_pairs,hendrycks_ethics,mmlu,medqa_4options,medqa,medqa5,medqa_template,medqa5_template,medmcqa,medmcqa_val,medmcqa_val_template,pubmedqa \
     --device cuda \
     --batch_size auto:4 \
