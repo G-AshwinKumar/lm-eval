f3ed0e3d0319b60a259bb487e8e665610bbffa23
pbernabeu <pablo.bernabeu@estudiantat.upc.edu>
2024-02-23 13:45:47 +0100

    Updated Eval Table Merger

--- utils/format_table.sh
+++ utils/format_table.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 
 # Check if three arguments are provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 commit_tag cesga_user job_pid"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 path filename"
     exit 1
 fi
 
 # Extract arguments
-commit_tag="$1"
-cesga_user="$2"
-job_pid="$3"
-
-path=""
-output_file="${path}formatted_out.o"
-
-# Construct the full path
-full_path="${path}job_${commit_tag}.o"
-
-# Check if the .o file exists
-if [ ! -f "$full_path" ]; then
-    echo "Directory does not exist: $full_path"
-    exit 1
-fi
+path="./$1"
+filename="$2"
 
 # Navigate to the specified directory
 cd "$path" || exit 1
 
 # Find the .o file and extract the table
-table=$(grep -E "^\|" *.o | awk -F'|' '/Tasks/ {p=1} p')
+table=$(grep -E "^\|" ${filename}.txt | awk -F'|' '/Tasks/ {p=1} p')
 
 # Process the table and create the reduced version
 reduced_table=$(echo "$table" | awk -F'|' 'BEGIN{OFS="|"} {if (NR==2) {printf "|%s|%s|%s---%s|\n", $2, $6, $7, $9} else {printf "|%s|%s|%s ± %s|\n", $2, $6, $7, $9}}')
@@ -43,6 +30,8 @@ LC_NUMERIC=C stderr=$(echo "$selected_rows" | awk -F' ± ' '{gsub(/,/, ".", $2);
 new_row="| - medicine       |acc            |$mean ± $stderr|"
 
 # Write the reduced table to the output file
+output_file="${filename}_formatted.o"
+
 echo "$reduced_table" >| "$output_file"
 echo "$new_row" >> "$output_file"
 
--- utils/table_merger.py
+++ utils/table_merger.py
@@ -1,83 +1,101 @@
+import os
 import pandas as pd
-import shutil
+import subprocess
 import sys
+from io import StringIO
 from pathlib import Path
 
-
 def read_table(file):
-    try:
-        table = pd.read_table(file, sep='|', skiprows=[1]).iloc[:, 1:-1]
-        table.columns = table.columns.str.strip()
-        table = table.map(lambda x: x.rstrip() if isinstance(x, str) else x)
-        return table
-    except:
-        print(f"Failed to load {file}")
-        return pd.DataFrame()
+    table = pd.read_table(file, sep='|', skiprows=[1]).iloc[:, 1:-1]
+    table.columns = table.columns.str.strip()
+    table = table.map(lambda x: x.rstrip() if isinstance(x, str) else x)
+    return table
+
+def read_tables(file):
+    # Read the entire file into a string
+    with open(file, 'r') as f:
+        content = f.read()
+    
+    # Split the content based on the second table's header
+    header = "|      Groups      |    Metric     |Value  ± Stderr|\n|------------------|---------------|-----: ± -----:|"
+    first_table, second_table = content.split(header)
+    
+    first_table = read_table(StringIO(first_table))
+    if second_table:
+        second_table = read_table(StringIO(header + "\n" + second_table))
+
+    return first_table, second_table
+
+def process_table(task_dict, table, method_column):
+    table = table.rename(columns={'Groups': 'Tasks', 'Value  ± Stderr': method_column})
+
+    last_task = None
+    for i, row in table.iterrows():
+        if type(row['Tasks']) == str and len(row['Tasks']) > 0:
+            if "Groups" in row['Tasks']:
+                break
+            if row['Tasks'] not in task_dict:
+                task_dict[row['Tasks']] = {row['Metric']: {method_column: row[method_column]}}
+            else:
+                if row['Metric'] not in task_dict[row['Tasks']]:
+                    task_dict[row['Tasks']][row['Metric']] = {method_column: row[method_column]}
+                else:
+                    task_dict[row['Tasks']][row['Metric']][method_column] = row[method_column]
+            last_task = row['Tasks']
+        else:
+            if last_task not in task_dict:
+                task_dict[last_task] = {row['Metric']: {method_column: row[method_column]}}
+            elif type(row['Metric']) == str and len(row['Metric']) > 0:
+                if row['Metric'] not in task_dict[last_task]:
+                    task_dict[last_task][row['Metric']] = {method_column: row[method_column]}
+                else:
+                    task_dict[last_task][row['Metric']][method_column] = row[method_column]
+    
+    return task_dict
+
+def table_to_md(task_dict):
+    merged = pd.DataFrame()
+    for task, metrics in task_dict.items():
+        # Transform the dictionary into a table
+        for i, (metric, values) in enumerate(metrics.items()):
+            row = pd.DataFrame({'Tasks': ["" if i else task], 'Metric': [metric]})
+            for k, v in values.items():
+                row[k] = v
+            merged = pd.concat([merged, row])
+    
+    return merged
 
 
 # Parse the arguments
-new_method = sys.argv[1]
-new_table_path = sys.argv[2]
-
-table_path = '/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/eval_results/evaluation_results.md'
-table = read_table(table_path)
+folder = sys.argv[1]
 
 task_dict = {}
-last_task = None
+group_dict = {}
 
-# Iterate through the table and create a dictionary of tasks and metrics
-for i, row in table.iterrows():
-    # If Tasks is not empty add it to the dictionary
-    if type(row['Tasks']) == str and len(row['Tasks']) > 0:
-        task_dict[row['Tasks']] = {row['Metric']: {c: row[c] for c in table.columns[2:]}}
-        last_task = row['Tasks']
-    # If Tasks is empty and Metric is not empty, add the metric to the last task
-    elif type(row['Metric']) == str and len(row['Metric']) > 0:
-        task_dict[last_task][row['Metric']] = {c: row[c] for c in table.columns[2:]}
+# Read the shots folder
+for file in os.listdir(folder):
+    if file.endswith('.txt'):
+        # Get the file name without the extension
+        filename = Path(file).stem
+        parts = filename.split('_')
+        commit, shots, method = parts[0], parts[1], '_'.join(parts[2:])
 
-new_table = read_table(new_table_path).rename(columns={'Value  ± Stderr': new_method})
-last_task = None
+        print(f'Processing {filename}...')
 
-# Iterate through the new_table adding the new method to the task_dict
-for i, row in new_table.iterrows():
-    if type(row['Tasks']) == str and len(row['Tasks']) > 0:
-        if "Groups" in row['Tasks']:
-            break
-        if row['Tasks'] not in task_dict:
-            task_dict[row['Tasks']] = {row['Metric']: {new_method: row[new_method]}}
-        else:
-            if row['Metric'] not in task_dict[row['Tasks']]:
-                task_dict[row['Tasks']][row['Metric']] = {new_method: row[new_method]}
-            else:
-                task_dict[row['Tasks']][row['Metric']][new_method] = row[new_method]
-        last_task = row['Tasks']
-    else:
-        if last_task not in task_dict:
-            task_dict[last_task] = {row['Metric']: {new_method: row[new_method]}}
-        elif type(row['Metric']) == str and len(row['Metric']) > 0:
-            if row['Metric'] not in task_dict[last_task]:
-                task_dict[last_task][row['Metric']] = {new_method: row[new_method]}
-            else:
-                task_dict[last_task][row['Metric']][new_method] = row[new_method]
+        result = subprocess.run(['./format_table.sh', folder, filename], capture_output=True, text=True)
 
-second_table_pos = i + 2
+        print(result.stdout.strip())
 
-# Create a new table from the dictionary
-merged = pd.DataFrame()
-for task, metrics in task_dict.items():
-    # Transform the dictionary into a table
-    for i, (metric, values) in enumerate(metrics.items()):
-        row = pd.DataFrame({'Tasks': ["" if i else task], 'Metric': [metric]})
-        for k, v in values.items():
-            row[k] = v
-        merged = pd.concat([merged, row])
+        first_table, second_table = read_tables(os.path.join(folder, result.stdout.strip()))
+        method_column = f'{method} [{commit}]'
 
-# Append second table to the merged table
-if second_table_pos < len(new_table):
-    merged = pd.concat([merged, new_table.iloc[second_table_pos:]])
+        task_dict = process_table(task_dict, first_table, method_column)
+        group_dict = process_table(group_dict, second_table, method_column)
 
-# Copy the old table to a new file
-shutil.copy(table_path, Path(table_path).parent / 'old_evaluation_results.md')
+task_table = table_to_md(task_dict)
+group_table = table_to_md(group_dict)
 
-with open(table_path, 'w+') as file:
-    file.write(merged.to_markdown(index=False).replace('nan', ''))
+with open(f'{shots}.md', 'w+') as file:
+    file.write(task_table.to_markdown(index=False).replace('nan', ''))
+    file.write("\n")
+    file.write(group_table.to_markdown(index=False).replace('nan', ''))
