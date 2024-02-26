c0e4bb80b1bb3918a3864ec8e93d3ed508cb1c06
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-02-26 10:19:53 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> openchat_3.5 test

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -6,9 +6,8 @@
 #SBATCH -o slurm_output/out.txt # Name of stdout output file(%j expands to jobId)
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
 #SBATCH --gres=gpu:a100:1   # Request 1 GPU of 2 available on an average A100 node
-#SBATCH --exclusive         # No other jobs allowed in our gpu
 #SBATCH -c 32               # Cores per task requested
-#SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 00:15:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
 MODEL_NAME="openchat_3.5"
@@ -19,7 +18,7 @@ CURRENT_DIR=$(pwd)
 echo "Current directory: '$CURRENT_DIR'"
 
 module load singularity/3.9.7
-singularity exec -B /mnt -B $CURRENT_DIR/tasks:/home/heka_eval/llm-evaluation-harness/lm-eval/tasks --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm_cuda118_new.sif \
+singularity exec -B /mnt -B $CURRENT_DIR/tasks:/home/llm-evaluation-harness/lm_eval/tasks --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm_cuda118_new2.sif \
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
@@ -27,5 +26,4 @@ singularity exec -B /mnt -B $CURRENT_DIR/tasks:/home/heka_eval/llm-evaluation-ha
     --tasks toxigen_generation,toxigen_generation_asian,toxigen_generation_black,toxigen_generation_chinese,toxigen_generation_jewish,toxigen_generation_latino,toxigen_generation_lgbtq,toxigen_generation_middle_east,toxigen_generation_mental_dis,toxigen_generation_mexican,toxigen_generation_muslim,toxigen_generation_native_american,toxigen_generation_physical_dis,toxigen_generation_women,bold,bold_american_actors,bold_american_actresses,bold_asian_americans,bold_buddhism,bold_islam,bold_left_wing,bold_hispanic_and_latino_americans,bold_european_americans,bold_african_americans,bold_judaism,bold_atheism,bold_christianity,bold_sikhism,bold_hinduism,truthfulqa_mc2,crows_pairs,hendrycks_ethics,mmlu,multimedqa,medqa_4options,medqa,medqa5,medqa_template,medqa5_template,medmcqa,medmcqa_val,medmcqa_val_template,pubmedqa \
     --device cuda \
     --batch_size auto:4 \
-    --num_fewshot 0 && \
-    utils/format_and_append.sh "${COMMIT_TAG}" "${USER}" "${JOB_PID}" "${MODEL_NAME}"'
+    --num_fewshot 0'
