362aebd11e619d10555ddb92183904179cd07365
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-02-29 13:17:40 +0100

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias5.sh> CausalLM-14B med_and_bias5

--- sbatch_vllm_med_and_bias5.sh
+++ sbatch_vllm_med_and_bias5.sh
@@ -8,10 +8,10 @@
 #SBATCH --gres=gpu:a100:2   # Request 1 or 2 GPU of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
 #SBATCH -c 64               # Cores per task requested
-#SBATCH -t 10:00:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node 
 
-MODEL_NAME="BioMistral-7B"
+MODEL_NAME="CausalLM-14B"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -23,7 +23,7 @@ singularity exec -B /mnt -B $CURRENT_DIR/tasks:/home/lm-evaluation-harness/lm-ev
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
-    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=2,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.8 \
+    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=2,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.75 \
     --tasks crows_pairs,hendrycks_ethics,multimedqa,medqa,medqa5,medqa_template,medqa5_template,medmcqa_val,medmcqa_val_template \
     --device cuda \
     --batch_size auto:4 \
