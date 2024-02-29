9d4db43f3fe5d9f8c81030f3969115b877ac0772
annariasdu <anna.duart@bsc.es>
2024-02-29 11:06:42 +0100

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias0.sh> BioMistral-7B 0 shot

--- sbatch_vllm_med_and_bias0.sh
+++ sbatch_vllm_med_and_bias0.sh
@@ -10,7 +10,7 @@
 #SBATCH -t 00:10:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="CausalLM-14B"
+MODEL_NAME="BioMistral-7B"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -22,7 +22,7 @@ singularity exec -B /mnt -B $CURRENT_DIR/tasks:/home/lm-evaluation-harness/lm_ev
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
-    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.9 \
+    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.8\
     --tasks bias_and_toxicity,multimedqa,mmlu,medqa,medqa5,medqa_template,medqa5_template,medmcqa,medmcqa_val,medmcqa_val_template \
     --device cuda \
     --batch_size auto:4 \
