b1adbe21c33f8a01f81bcd7f0db7ad9aeefb68d3
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-01 22:07:31 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> meditron-7b 0 shot

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -19,12 +19,12 @@ echo "Current directory: '$CURRENT_DIR'"
 echo $MODEL_PATH
 
 module load singularity/3.9.7
-singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm_cuda118_new2.sif \
+singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm032_cuda118.sif \
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
-    TORCH_USE_CUDA_DSA=1 python -m lm_eval \
+    CUDA_LAUNCH_BLOCKING=1 TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model hf-auto \
     --model_args pretrained='${MODEL_PATH}' \
     --tasks multimedqa,toxigen_generation,toxigen_generation_asian,toxigen_generation_black,toxigen_generation_chinese,toxigen_generation_jewish,toxigen_generation_latino,toxigen_generation_lgbtq,toxigen_generation_middle_east,toxigen_generation_mental_dis,toxigen_generation_mexican,toxigen_generation_muslim,toxigen_generation_native_american,toxigen_generation_physical_dis,toxigen_generation_women,bold,bold_american_actors,bold_american_actresses,bold_asian_americans,bold_buddhism,bold_islam,bold_left_wing,bold_hispanic_and_latino_americans,bold_european_americans,bold_african_americans,bold_judaism,bold_atheism,bold_christianity,bold_sikhism,bold_hinduism,truthfulqa_mc2,crows_pairs,hendrycks_ethics,mmlu,medqa,medqa5,medqa_template,medqa5_template,medmcqa,medmcqa_val,medmcqa_val_template \
-    --device cuda \
+    --device cuda:0 \
     --batch_size auto:4 \
     --num_fewshot 0'
