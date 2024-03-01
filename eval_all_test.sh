e198c391fcd1912dc713f60edfd570f40d590af1
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-01 11:52:17 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> Nous-Hermes-2-SOLAR-10.7B, 0-shot test multimedqa

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -7,10 +7,10 @@
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
 #SBATCH --gres=gpu:a100:1   # Request 1 GPU of 2 available on an average A100 node
 #SBATCH -c 32               # Cores per task requested
-#SBATCH -t 00:05:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="openchat_3.5"
+MODEL_NAME="Nous-Hermes-2-SOLAR-10.7B"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -18,11 +18,11 @@ CURRENT_DIR=$(pwd)
 echo "Current directory: '$CURRENT_DIR'"
 
 module load singularity/3.9.7
-singularity exec -B /mnt --overlay $CURRENT_DIR/tasks:/home/lm-evaluation-harness/lm_eval/tasks --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm_cuda118.sif \
+singularity exec -B /mnt --overlay $CURRENT_DIR/tasks:/home/lm-evaluation-harness/lm_eval/tasks --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm_cuda118_new2.sif \
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
-    --model_args pretrained="${MODEL_PATH}",tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.85 \
-    --tasks list \
+    --model_args pretrained="${MODEL_PATH}",tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.65 \
+    --tasks multimedqa \
     --device cuda \
     --batch_size auto:4'
