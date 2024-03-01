69a3ddf7db5498f9892be8a221faca7ceb561f48
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-01 12:15:38 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> Nous-Hermes-2-SOLAR-10.7B, 0-shot test multimedqa

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -10,7 +10,7 @@
 #SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="Nous-Hermes-2-SOLAR-10.7B"
+MODEL_NAME="Nous-Hermes-2-SOLAR-10.7"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
