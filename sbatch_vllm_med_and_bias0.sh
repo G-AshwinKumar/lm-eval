ac258b93c49953e6989f5f95d3e3a1a0598c7c7a
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-01 21:32:25 +0100

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias0.sh> gemma-7b 0-shot

--- sbatch_vllm_med_and_bias0.sh
+++ sbatch_vllm_med_and_bias0.sh
@@ -11,7 +11,7 @@
 #SBATCH -t 10:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="gemma-7B"
+MODEL_NAME="gemma-7b"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
