f36c4c15c4d7bd27358c41f4624171172ea0e145
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-04 10:35:21 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> internlm2-chat-7b test winobias

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -7,10 +7,10 @@
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
 #SBATCH --gres=gpu:a100:1   # Request 1 or 2 GPUs of 2 available on an average A100 node
 #SBATCH -c 32               # Cores per task requested
-#SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 00:10:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="meditron-7b"
+MODEL_NAME="internlm2-chat-7b"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -22,9 +22,9 @@ module load singularity/3.9.7
 singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm032_cuda118.sif \
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     CUDA_LAUNCH_BLOCKING=1 TORCH_USE_CUDA_DSA=1 python -m lm_eval \
-    --model hf-auto \
-    --model_args pretrained='${MODEL_PATH}' \
-    --tasks winobias \
+    --model vllm \
+    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.85 \
+    --tasks winobias,winobias_stereotypical,winobias_anti_stereotypical \
     --device cuda:0 \
     --batch_size auto:4 \
     --num_fewshot 0'
