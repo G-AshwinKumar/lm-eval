21ae7b4023cdbe6d0445e70a5674c28d81d47ed8
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-04 23:09:40 +0100

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias0.sh>internlm2-chat-7b 0-shot

--- sbatch_vllm_med_and_bias0.sh
+++ sbatch_vllm_med_and_bias0.sh
@@ -8,7 +8,7 @@
 #SBATCH --gres=gpu:a100:2   # Request 1 or 2 GPUs of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
 #SBATCH -c 64               # Cores per task requested
-#SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 12:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
 MODEL_NAME="internlm2-chat-7b"
