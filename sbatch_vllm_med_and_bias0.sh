a340340fd8cb3986bf922ba6940884be0e2184c7
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-09 13:05:34 +0100

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias0.sh> MMedLM2 0-shot

--- sbatch_vllm_med_and_bias0.sh
+++ sbatch_vllm_med_and_bias0.sh
@@ -8,7 +8,7 @@
 #SBATCH --gres=gpu:a100:1   # Request 1 or 2 GPUs of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
 #SBATCH -c 32               # Cores per task requested
-#SBATCH -t 12:00:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
 MODEL_NAME="MMedLM2"
@@ -25,5 +25,5 @@ singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Sin
     --model vllm \
     --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.7 \
     --tasks multimedqa,toxigen_generation,toxigen_generation_asian,toxigen_generation_black,toxigen_generation_chinese,toxigen_generation_jewish,toxigen_generation_latino,toxigen_generation_lgbtq,toxigen_generation_middle_east,toxigen_generation_mental_dis,toxigen_generation_mexican,toxigen_generation_muslim,toxigen_generation_native_american,toxigen_generation_physical_dis,toxigen_generation_women,bold,bold_american_actors,bold_american_actresses,bold_asian_americans,bold_buddhism,bold_islam,bold_left_wing,bold_hispanic_and_latino_americans,bold_european_americans,bold_african_americans,bold_judaism,bold_atheism,bold_christianity,bold_sikhism,bold_hinduism,truthfulqa_mc2,crows_pairs,hendrycks_ethics,mmlu,medqa,medqa5,medqa_template,medqa5_template,medmcqa,medmcqa_val,medmcqa_val_template \
-    --batch_size auto:4 \
+    --batch_size auto:16 \
     --num_fewshot 0'
