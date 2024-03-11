e7172730b5e66024c8debb1e0577713180396b56
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-11 11:17:06 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> openchat_3.5 test new tasks winobias, mimic_repsum, bbq

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -23,7 +23,7 @@ singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Sin
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     CUDA_LAUNCH_BLOCKING=1 TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
-    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.7 \
+    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.6 \
     --tasks winobias,winobias_stereotypical,winobias_anti_stereotypical,mimic_repsum,bbq \
     --device cuda:0 \
     --batch_size auto:4 \
