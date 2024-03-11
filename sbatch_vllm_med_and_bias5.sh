3b805610dc220d0922ef2dcd6f0de6db4b74b47b
annariasdu <anna.duart@bsc.es>
2024-03-11 12:51:20 +0100

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias5.sh> Yi-9B 5 shot

--- sbatch_vllm_med_and_bias5.sh
+++ sbatch_vllm_med_and_bias5.sh
@@ -11,7 +11,7 @@
 #SBATCH -t 14:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node 
 
-MODEL_NAME="PMC_LLaMA_13B"
+MODEL_NAME="Yi-9B"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -24,7 +24,7 @@ singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Sin
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
     --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=2,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.78 \
-    --tasks crows_pairs,hendrycks_ethics,multimedqa,medqa,medqa5,medqa_template,medqa5_template,medmcqa_val,medmcqa_val_template \
+    --tasks multimedqa,mmlu,medqa,medqa5,medqa_template,medqa5_template,medmcqa,medmcqa_val,medmcqa_val_template \
     --device cuda \
     --batch_size 32 \
     --num_fewshot 5'
