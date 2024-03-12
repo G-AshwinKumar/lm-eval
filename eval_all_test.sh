74983184823939be8e9a0f993ed6ebc77e327a00
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-12 10:40:18 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> openchat_3.5 test new tasks winobias, bbq

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -6,6 +6,7 @@
 #SBATCH -o slurm_output/out.txt # Name of stdout output file(%j expands to jobId)
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
 #SBATCH --gres=gpu:a100:1   # Request 1 or 2 GPUs of 2 available on an average A100 node
+#SBATCH --exclusive         # "no other jobs" in our gpus
 #SBATCH -c 32               # Cores per task requested
 #SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
@@ -23,7 +24,7 @@ singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Sin
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     CUDA_LAUNCH_BLOCKING=1 TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
-    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.6 \
+    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.7 \
     --tasks winobias,winobias_stereotypical,winobias_anti_stereotypical,mimic_repsum,bbq \
     --device cuda:0 \
     --batch_size auto:4 \
