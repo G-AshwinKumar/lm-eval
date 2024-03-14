2d5d7a0328c9687aea96ce9fe697056ddfd3c7f6
Pablo Bernabeu Perez <pablo.bernabeu@estudiantat.upc.edu>
2024-03-14 12:45:45 +0100

    LAUNCH_CESGA <sbatch new_eval_all.sh> medalpaca-7b 5-shot multimedqa

--- new_eval_all.sh
+++ new_eval_all.sh
@@ -6,7 +6,7 @@
 #SBATCH -o slurm_output/out.txt # Name of stdout output file(%j expands to jobId)
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
 #SBATCH --gres=gpu:a100:1   # Request 1 GPU of 2 available on an average A100 node
-#SBATCH -c 32               # Cores per task requested
+#SBATCH -c 32               # Cores per task requested.
 #SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
