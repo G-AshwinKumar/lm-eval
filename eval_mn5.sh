7775e46e472d5013f7c423e0cc82f0b431a95d40
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-15 12:04:00 +0100

    Update eval_mn5.sh

--- eval_mn5.sh
+++ eval_mn5.sh
@@ -18,9 +18,10 @@ MODEL_PATH="/gpfs/tapes/MN4/projects/bsc70/hpai/storage/data/heka/Models/$MODEL_
 CURRENT_DIR=$(pwd)
 echo "Current directory: '$CURRENT_DIR'"
 
-module load singularity
+module load singularity/3.9.7
 singularity exec --nv /gpfs/scratch/bsc70/hpai/storage/projects/heka/singularity/lm_eval_harness_vllm032_cuda118_research.sif \
-    bash -c 'export HF_DATASETS_CACHE="/gpfs/scratch/bsc70/hpai/storage/projects/heka/hf_caches/hf_cache_cns10888" && \
+    bash -c 'export HF_HUB_OFFLINE=1 && \
+    export HF_DATASETS_CACHE="/gpfs/scratch/bsc70/hpai/storage/projects/heka/hf_caches/hf_cache_cns10888" && \
     export HF_HOME="/gpfs/scratch/bsc70/hpai/storage/projects/heka/hf_data" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
