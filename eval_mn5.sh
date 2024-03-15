4de984326e3aed82bd8ee4a11ce55ea88395dacd
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-15 20:58:41 +0100

    LAUNCH_MN5_GPP <sbatch eval_mn5.sh> test

--- eval_mn5.sh
+++ eval_mn5.sh
@@ -5,6 +5,8 @@
 #SBATCH -J eval-mn5      # Job name
 #SBATCH -o slurm_output/out.txt       # Name of stdout output file(%j expands to jobId)
 #SBATCH -e slurm_output/err.txt       # Name of stderr output file(%j expands to jobId)
+#SBATCH --gres=gpu:h100:1   
+#SBATCH -c 32               # Cores per task requested
 #SBATCH -t 00:10:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --account bsc70  
 #SBATCH --qos=gp_bsccs
