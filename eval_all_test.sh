ee303025afd97cba4e623e452c353ccd8afd20a0
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-18 13:52:07 +0100

    LAUNCH_CESGA <sbatch eval_all_test.sh> Yi-9B mimic

--- eval_all_test.sh
+++ eval_all_test.sh
@@ -6,11 +6,12 @@
 #SBATCH -o slurm_output/out.txt # Name of stdout output file(%j expands to jobId)
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
 #SBATCH --gres=gpu:a100:1   # Request 1 or 2 GPUs of 2 available on an average A100 node
+#SBATCH --exclusive
 #SBATCH -c 32               # Cores per task requested
 #SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="openchat_3.5"
+MODEL_NAME="Yi-9B"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -19,7 +20,7 @@ echo "Current directory: '$CURRENT_DIR'"
 echo $MODEL_PATH
 
 module load singularity/3.9.7
-singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm032_cuda118_research.sif \
+singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm032_cuda118_research2.sif \
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     CUDA_LAUNCH_BLOCKING=1 TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
