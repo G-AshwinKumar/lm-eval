59b79841e919aa873b766dcc409a37f88d94a5d1
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-18 10:50:23 +0100

    LAUNCH_CESGA <sbatch new_eval_all.sh> MMedLM2 5-shot multimedqa

--- new_eval_all.sh
+++ new_eval_all.sh
@@ -22,8 +22,8 @@ singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Sin
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
-    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.65 \
+    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.6 \
     --tasks multimedqa \
     --device cuda \
-    --batch_size 16 \
+    --batch_size 8 \
     --num_fewshot 5'
