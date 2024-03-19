998db3c7583c67e004224cce329fc7a0b8752048
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-19 16:59:47 +0100

    LAUNCH_CESGA <sbatch new_eval_all.sh> MMedLM2 5-shot multimedqa test

--- new_eval_all.sh
+++ new_eval_all.sh
@@ -6,9 +6,8 @@
 #SBATCH -o slurm_output/out.txt # Name of stdout output file(%j expands to jobId)
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
 #SBATCH --gres=gpu:a100:1   # Request 1 GPU of 2 available on an average A100 node
-#SBATCH --exclusive
 #SBATCH -c 32               # Cores per task requested.
-#SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 00:10:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
 MODEL_NAME="MMedLM2"
@@ -19,7 +18,7 @@ CURRENT_DIR=$(pwd)
 echo "Current directory: '$CURRENT_DIR'"
 
 module load singularity/3.9.7
-singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm032_cuda118_research2.sif \
+singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm032_cuda118_research.sif \
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
