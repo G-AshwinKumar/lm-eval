085473ac10f5a057c49b3dfba3674bb251f99d01
annariasdu <anna.duart@bsc.es>
2024-03-20 16:25:40 +0100

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias5.sh> Yi-9B 5 shot mir2023_en

--- sbatch_vllm_med_and_bias5.sh
+++ sbatch_vllm_med_and_bias5.sh
@@ -11,7 +11,7 @@
 #SBATCH -t 12:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node 
 
-MODEL_NAME="PMC_LLaMA_13B"
+MODEL_NAME="Yi-9B"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -19,12 +19,12 @@ CURRENT_DIR=$(pwd)
 echo "Current directory: '$CURRENT_DIR'"
 
 module load singularity/3.9.7
-singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness_vllm032_cuda118_research.sif \
+singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness042_vllm032_cuda118.sif \
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
     --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=2,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.6 \
-    --tasks bias_and_toxicity \
+    --tasks mir2023_en \
     --device cuda \
     --batch_size auto:4 \
     --num_fewshot 5'
