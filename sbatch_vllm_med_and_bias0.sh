31f086f8b51bfe9a59447e92c38b188e585076f7
annariasdu <anna.duart@bsc.es>
2024-03-21 12:15:46 +0100

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias0.sh> MMedLM2 0 shot MIR_en

--- sbatch_vllm_med_and_bias0.sh
+++ sbatch_vllm_med_and_bias0.sh
@@ -8,10 +8,10 @@
 #SBATCH --gres=gpu:a100:1   # Request 1 or 2 GPUs of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
 #SBATCH -c 32               # Cores per task requested
-#SBATCH -t 00:20:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 00:15:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="Yi-9B"
+MODEL_NAME="MMedLM2"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
