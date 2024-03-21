0d6b9bcf1ccfc1a4e3b204cc5c42923d61244e35
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-21 14:58:08 +0100

    LAUNCH_MN5_ACC <sbatch eval_mn5.sh> test

--- eval_mn5.sh
+++ eval_mn5.sh
@@ -25,7 +25,7 @@ singularity exec -B /gpfs/projects/bsc70/heka \
                  -B /gpfs/projects/bsc70/heka/repos/tmp_eval_harness/lm-evaluation-harness/lm_eval/api/task.py:/home/lm-evaluation-harness/lm_eval/api/task.py \
                  -B /gpfs/tapes/MN4/projects/bsc70/hpai/storage/data/heka/Models \
                  --nv /gpfs/projects/bsc70/heka/singularity/lm_eval_harness_c7b03ad_121_033_fix.sif \
-   bash -c 'export HF_HOME=/gpfs/projects/bsc70/heka/repos/tmp_eval_harness/hf_cache && export HF_DATASETS_CACHE="/gpfs/projects/bsc70/heka/repos/tmp_eval_harness/hf_cache" && \
+   bash -c 'export HF_HOME=/gpfs/projects/bsc70/heka/repos/tmp_eval_harness/hf_cache && export HF_DATASETS_CACHE="/gpfs/scratch/bsc70/hpai/storage/projects/heka/hf_caches/hf_cache" && \
     python /home/lm-evaluation-harness/lm_eval \
     --model vllm \
     --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,dtype=bfloat16,gpu_memory_utilization=0.8,data_parallel_size=1,max_model_len=8192 \
