5abc2cf496bf4daf7010d54309459fbce5ecc732
annariasdu <anna.duart@bsc.es>
2024-03-25 16:52:05 +0100

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias0.sh> SOLAR-10.7B-Instruct-v1.0 0 shot bbq

--- sbatch_vllm_med_and_bias0.sh
+++ sbatch_vllm_med_and_bias0.sh
@@ -11,7 +11,7 @@
 #SBATCH -t 00:30:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="Yi-9B"
+MODEL_NAME="SOLAR-10.7B-Instruct-v1.0"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -24,6 +24,6 @@ singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Sin
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
     --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=auto,gpu_memory_utilization=0.6 \
-    --tasks winobias \
+    --tasks bbq \
     --batch_size auto:4 \
     --num_fewshot 0'
