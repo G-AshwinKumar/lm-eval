894fb08d4f86038266e10b87954e39182364854b
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-03-26 10:30:57 +0100

    LAUNCH_CESGA <sbatch sbatch_vllm_bias0.sh> openchat_3.5 0 shot ethos

--- sbatch_vllm_bias0.sh
+++ sbatch_vllm_bias0.sh
@@ -23,7 +23,7 @@ singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Sin
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
     --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.65 \
-    --tasks advanced_ai_risk \
+    --tasks ethos,ethos_generation_religion,ethos_generation_disability \
     --device cuda \
     --batch_size auto:4 \
     --num_fewshot 0'
