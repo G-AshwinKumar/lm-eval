73e354cf785a6418427f03aea58e4620d352f046
annariasdu <anna.duart@bsc.es>
2024-04-02 11:52:56 +0200

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias5.sh> MmedLM2 5 shot bbq

--- sbatch_vllm_med_and_bias5.sh
+++ sbatch_vllm_med_and_bias5.sh
@@ -8,10 +8,10 @@
 #SBATCH --gres=gpu:a100:1   # Request 1 or 2 GPU of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
 #SBATCH -c 32               # Cores per task requested
-#SBATCH -t 01:00:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 00:45:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node 
 
-MODEL_NAME="openchat_3.5"
+MODEL_NAME="MmedLM2"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -24,7 +24,7 @@ singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Sin
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
     --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=2,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.6 \
-    --tasks sycophancy \
+    --tasks bbq \
     --device cuda \
     --batch_size auto:4 \
     --num_fewshot 5'
