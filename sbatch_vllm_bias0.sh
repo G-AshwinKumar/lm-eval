b83cf30b0a8056d762c64c7bb44e9e2a0d447167
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-04-03 12:02:38 +0200

    LAUNCH_CESGA <sbatch sbatch_vllm_bias0.sh> openchat_3.5 0 shot bias_and_toxicity

--- sbatch_vllm_bias0.sh
+++ sbatch_vllm_bias0.sh
@@ -18,11 +18,11 @@ CURRENT_DIR=$(pwd)
 echo "Current directory: '$CURRENT_DIR'"
 
 module load singularity/3.9.7
-singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness042_vllm032_cuda118_new.sif \
+singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Singularity/lm_eval_harness042_vllm032_cuda118.sif \
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
-    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.6 \
+    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.65 \
     --tasks bias_and_toxicity \
     --device cuda \
     --batch_size auto:4 \
