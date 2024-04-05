cb35b6ea57001ea6cbdb28de467b0048c00caf62
annariasdu <anna.duart@bsc.es>
2024-04-05 11:56:21 +0200

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias0.sh> Nous-Hermes-2-SOLAR-10.7B 0 shot sycophancy all

--- sbatch_vllm_med_and_bias0.sh
+++ sbatch_vllm_med_and_bias0.sh
@@ -8,10 +8,10 @@
 #SBATCH --gres=gpu:a100:1   # Request 1 or 2 GPUs of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
 #SBATCH -c 32               # Cores per task requested
-#SBATCH -t 00:30:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 01:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
-MODEL_NAME="SOLAR-10.7B-Instruct-v1.0"
+MODEL_NAME="Nous-Hermes-2-SOLAR-10.7B"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
 MODEL_PATH="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Models_Trained/llm/$MODEL_NAME"
 # use pwd
@@ -24,6 +24,6 @@ singularity exec -B /mnt --nv /mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/Sin
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
     --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,trust_remote_code=True,dtype=auto,gpu_memory_utilization=0.8 \
-    --tasks winogender \
+    --tasks sycophancy \
     --batch_size auto:4 \
     --num_fewshot 0'
