9d93d7eeb10486609d169e5198f899f7565a8500
annariasdu <anna.duart@bsc.es>
2024-04-05 16:05:37 +0200

    LAUNCH_CESGA <sbatch sbatch_vllm_med_and_bias0.sh> MMedLM2 0 shot sycophancy all

--- sbatch_vllm_med_and_bias0.sh
+++ sbatch_vllm_med_and_bias0.sh
@@ -8,7 +8,7 @@
 #SBATCH --gres=gpu:a100:1   # Request 1 or 2 GPUs of 2 available on an average A100 node
 #SBATCH --exclusive         # No other jobs allowed in our gpu
 #SBATCH -c 32               # Cores per task requested
-#SBATCH -t 01:00:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 01:03:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
 MODEL_NAME="MMedLM2"
