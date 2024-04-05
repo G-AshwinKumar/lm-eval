27f1a2c347fe3890be9732df5b7427b3d1d46af7
Pablo Agustin Martin Torres <pablo.martin@bsc.es>
2024-04-05 11:10:48 +0200

    LAUNCH_CESGA <sbatch sbatch_vllm_hf_leaderboard.sh> CausalLM-14B hf leaderboard

--- sbatch_vllm_hf_leaderboard.sh
+++ sbatch_vllm_hf_leaderboard.sh
@@ -6,9 +6,8 @@
 #SBATCH -o slurm_output/out.txt # Name of stdout output file(%j expands to jobId)
 #SBATCH -e slurm_output/err.txt # Name of stderr output file(%j expands to jobId)
 #SBATCH --gres=gpu:a100:1   # Request 1 or 2 GPUs of 2 available on an average A100 node
-#SBATCH --exclusive         # No other jobs allowed in our gpu
 #SBATCH -c 32               # Cores per task requested
-#SBATCH -t 06:00:00         # Run time (hh:mm:ss) - 30 min
+#SBATCH -t 02:00:00         # Run time (hh:mm:ss) - 30 min
 #SBATCH --mem=247G          # Memory per node
 
 MODEL_NAME="CausalLM-14B"
@@ -23,7 +22,7 @@ singularity exec -B /mnt -B $CURRENT_DIR/tasks:/home/lm-evaluation-harness/lm_ev
     bash -c 'export HF_DATASETS_CACHE="/mnt/lustre/scratch/nlsas/home/res/cns10/SHARE/user_caches/hf_cache_'${USER}'" && \
     TORCH_USE_CUDA_DSA=1 python -m lm_eval \
     --model vllm \
-    --model_args pretrained="${MODEL_PATH}",tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.8 \
+    --model_args pretrained="${MODEL_PATH}",tensor_parallel_size=1,trust_remote_code=True,dtype=bfloat16,gpu_memory_utilization=0.5 \
     --tasks hf_leaderboard \
     --device cuda \
     --batch_size auto:4'
