9f38c970dc75b0d609d81907fa4378f6d82201d5
Ashwin Kumar Gururajan <ashwin.gururajan@bsc.es>
2024-04-07 15:12:09 +0200

    LAUNCH_MN5_ACC <sbatch eval_mn5.sh>c4ai-command-r-v01 medmcqa

--- eval_mn5.sh
+++ eval_mn5.sh
@@ -3,19 +3,19 @@
 # Example SLURM job script with SBATCH requesting GPUs
 #------------------------------------------------------
 #SBATCH -J eval-mn5      # Job name
-#SBATCH -o slurm_output/out.txt       # Name of stdout output file(%j expands to jobId)
-#SBATCH -e slurm_output/err.txt       # Name of stderr output file(%j expands to jobId)
-#SBATCH -n 1
-#SBATCH --gres=gpu:1 
-#SBATCH --exclusive         # No other jobs allowed in our gpu
-#SBATCH -c 32               # Cores per task requested
-#SBATCH -t 00:30:00         # Run time (hh:mm:ss) - 30 min
-#SBATCH --account bsc70  
+#SBATCH --account=bsc70
 #SBATCH --qos=acc_bsccs
+#SBATCH -o slurm_output/err.txt       # Name of stdout output file(%j expands to jobId)
+#SBATCH -e slurm_output/out.txt       # Name of stderr output file(%j expands to jobId)
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=80
+#SBATCH --time=02:00:00
+#SBATCH --gres=gpu:4
 
-MODEL_NAME="mistral7b-full_v6_med"
+
+MODEL_NAME="c4ai-command-r-v01"
 echo "Starting sbatch script at `date` for $MODEL_NAME"
-MODEL_PATH="/gpfs/tapes/MN4/projects/bsc70/hpai/storage/data/heka/Models/$MODEL_NAME"
+MODEL_PATH="/gpfs/projects/bsc70/heka/models/$MODEL_NAME"
 # use pwd
 CURRENT_DIR=$(pwd)
 echo "Current directory: '$CURRENT_DIR'"
@@ -31,9 +31,9 @@ singularity exec -B /gpfs/projects/bsc70/heka \
    bash -c 'export HF_HUB_OFFLINE=1 && export HF_HOME=/gpfs/scratch/bsc70/hpai/storage/projects/heka/hf_caches/hf_cache2 && export HF_DATASETS_CACHE="/gpfs/scratch/bsc70/hpai/storage/projects/heka/hf_caches/hf_cache2" && \
     python /home/lm-evaluation-harness/lm_eval \
     --model vllm \
-    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=1,dtype=bfloat16,gpu_memory_utilization=0.9,data_parallel_size=1,max_model_len=8192 \
-    --tasks mir2023_en \
+    --model_args pretrained='${MODEL_PATH}',tensor_parallel_size=4,dtype=bfloat16,gpu_memory_utilization=0.9,data_parallel_size=1,max_model_len=8192 \
+    --tasks medmcqa \
     --batch_size auto:4 \
     --num_fewshot 0 \
-    --output_path '${CURRENT_DIR}' \
+    --output_path '${CURRENT_DIR}/${COMMIT_TAG}.txt' \
     --log_samples'
