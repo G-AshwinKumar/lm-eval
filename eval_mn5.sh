65660a6c5206fd576023f4b0f711e6d3953aaea6
Ashwin Kumar Gururajan <ashwin.gururajan@bsc.es>
2024-04-07 15:25:04 +0200

    LAUNCH_MN5_ACC <sbatch eval_mn5.sh>c4ai-command-r-v01 medmcqa use new singularity image

--- eval_mn5.sh
+++ eval_mn5.sh
@@ -27,7 +27,7 @@ singularity exec -B /gpfs/projects/bsc70/heka \
                  -B /gpfs/tapes/MN4/projects/bsc70/hpai/storage/data/heka/Models \
                  -B /gpfs/projects/bsc70/heka/repos/tmp_eval_harness/lm-evaluation-harness/lm_eval/toxigen_generation/toxigen_generation.yaml:/home/lm-evaluation-harness/lm_eval/tasks/toxigen_generation/toxigen_generation.yaml \
                  -B /gpfs/projects/bsc70/heka/repos/tmp_eval_harness/lm-evaluation-harness/lm_eval/toxigen_generation/utils.py:/home/lm-evaluation-harness/lm_eval/tasks/toxigen_generation/utils.py \
-                 --nv /gpfs/scratch/bsc70/hpai/storage/projects/heka/singularity/lm_eval_harness042_vllm032_cuda118_new.sif \
+                 --nv /gpfs/scratch/bsc70/hpai/storage/projects/heka/singularity/lm-evaluation-harness/lmharness.sif \
    bash -c 'export HF_HUB_OFFLINE=1 && export HF_HOME=/gpfs/scratch/bsc70/hpai/storage/projects/heka/hf_caches/hf_cache2 && export HF_DATASETS_CACHE="/gpfs/scratch/bsc70/hpai/storage/projects/heka/hf_caches/hf_cache2" && \
     python /home/lm-evaluation-harness/lm_eval \
     --model vllm \
